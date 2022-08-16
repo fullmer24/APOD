cabc7e178695c3c63ecaab70de1e5c0353441391
Kade Fullmer <fullmer24@gmail.com>
2022-08-16 15:30:40 -0600

    Axios set up

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,6 +1,12 @@
 import Axios from 'axios'
-import { baseURL } from '../env'
-export const api = Axios.create({
-  baseURL,
-  timeout: 8000
+export const nasaApi = Axios.create({
+  baseURL: 'https://bcw-getter.herokuapp.com',
+  timeout: 4000,
+  params: {
+    url: 'https://api.nasa.gov/planetary/apod',
+    api_key: 'jZacntog0X5xMwam4Bdh7qVdmis6LhqI7ZlpGWpe'
+  }
 })
+
+
+// https://api.nasa.gov/planetary/apod?api_key=jZacntog0X5xMwam4Bdh7qVdmis6LhqI7ZlpGWpe
